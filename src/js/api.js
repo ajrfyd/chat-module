3b799930e3f39c19779df19d784fe521861ea629
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-02-27 15:05:33 +0900

    Modify: req url modified

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
-// const url = "https://chat.hkound.pe.kr";
-export const url = "http://localhost:8088";
+const url = "https://chat.hkound.pe.kr";
+// export const url = "http://localhost:8088";
 
 export const getUserInfo = async (nickName) =>
   await fetch(`${url}/user/search/${nickName}`).then((res) => res.json());
